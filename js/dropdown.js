eb9a2039534e2f8d8c32656c692e6e78f9213cc2
Rimpa Das <rimpa@itobuz.com>
2021-10-28 18:42:39 +0530

    modify

--- js/dropdown.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-    var navToggle = jQuery('#dropbtn');
-    var nav = jQuery('.dropdown-content');
-
-    navToggle.on('click', function(e) {
-
-        nav.slideToggle();
-    });
-})
