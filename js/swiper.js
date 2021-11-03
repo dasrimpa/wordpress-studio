c09054f25e92c582ed4c4af2c03c844d0746e880
Rimpa Das <rimpa@itobuz.com>
2021-11-03 17:51:27 +0530

    modify in slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,35 +1,18 @@
 $(document).ready(function() {
 
     $(function() {
-        var mySwiper = $('.swiper').swiper({
-            mode: 'horizontal',
-            watchActiveIndex: true,
-            loop: true,
-            pagination: '.swiper-pagination',
-            slidesPerView: 3,
-            centeredSlides: false,
-            paginationClickable: true,
-            autoplay: 2000,
-            spaceBetween: 35,
-            onSlideChangeStart: function(swiper) {
-                console.log('slide change start - before');
-                console.log(swiper);
-                console.log(swiper.activeIndex);
-
-                //before Event use it for your purpose
-            },
-            onSlideChangeEnd: function(swiper) {
-                console.log('slide change end - after');
-                console.log(swiper);
-                console.log(swiper.activeIndex);
-                //after Event use it for your purpose
-                if (swiper.activeIndex == 1) {
-                    //First Slide is active
-                    console.log('First slide active')
-                }
-            }
-        });
-
+        // var mySwiper = $('.swiper').swiper({
+        //     mode: 'horizontal',
+        //     watchActiveIndex: true,
+        //     slideperview: 3,
+        //     loop: true,
+        //     pagination: '.swiper-pagination',
+        //     slidesPerView: 4,
+        //     centeredSlides: false,
+        //     paginationClickable: true,
+        //     autoplay: 2000,
+        //     spaceBetween: 15,
+        // });
     })
 
 });
